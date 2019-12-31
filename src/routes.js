333745be488e2317ba5c26cffaaf1249ffe80437
ricardolimati <ricardodreamsites@gmail.com>
2019-12-30 22:15:51 -0300

    NODEJS - CRUD

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-    // Product.create({
-    //     Title: 'React Nativa',
-    //     description: 'Build Native app with',
-    //     url: 'https://github.com/facebook/react-native'
-    // });
-    return res.send("Ano Novo!");
-});
+const ProductController = require('./controllers/ProductController');
+
+routes.get('/products', ProductController.index);
+routes.post('/products', ProductController.store);
+routes.get('/products/:id', ProductController.show);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
